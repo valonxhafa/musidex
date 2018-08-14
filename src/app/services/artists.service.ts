2a0ddfa3e49434c932ede6f6c347cb80beb269ee
VXHAFA <valon.xhafa@vdab.be>
2018-08-14 14:23:56 +0200

    retrieve doc OK

--- src/app/services/artists.service.ts
+++ src/app/services/artists.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import { AngularFireObject, AngularFireList, AngularFireDatabase } from 'angularFire2/database';
 import { Artist } from '../models/artist';
-import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '../../../node_modules/angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Injectable()
 export class ArtistsService {
@@ -15,7 +15,6 @@ export class ArtistsService {
 
   constructor(private afs: AngularFirestore) {
     this.artistCollection = afs.collection<Artist>('Artists');
-
     this.artists = this.artistCollection.snapshotChanges().map(changes => {
       return changes.map(a => {
         const data = a.payload.doc.data() as Artist;
@@ -23,10 +22,28 @@ export class ArtistsService {
         return data;
       });
     });
+
+    this.getArtist();
   }
 
   public getArtists() {
     return this.artists;
+  }
+
+  getArtist() {
+    this.artistCollection.doc('M7mvTtd5TsBpqc1CsNUI').ref.get().then(function(doc) {
+      if (doc.exists) {
+        console.log('Document data:', doc.data());
+      } else {
+        console.log('No such document!');
+      }
+    }).catch(function(error) {
+      console.log('Error getting document:', error);
+    });
+  }
+
+  deleteArtist(artist: Artist) {
+    this.artistCollection.doc(artist.$key).delete();
   }
 
 }
